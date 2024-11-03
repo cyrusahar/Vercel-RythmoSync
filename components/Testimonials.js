b2251eb7415276641c558d7806f42d7f63e5563f
Cyrus Sahar <ffenjil@proton.me>
2024-11-03 17:43:33 +0530

    Create Testimonials.js

---
+++ components/Testimonials.js
@@ -0,0 +1,16 @@
+// components/Testimonials.js
+import styled from 'styled-components';
+
+const Testimonials = () => (
+  <Section>
+    <h2>Testimonials</h2>
+    <p>Display user reviews and testimonials for RhythmoSync.</p>
+  </Section>
+);
+
+export default Testimonials;
+
+const Section = styled.section`
+  padding: 2rem;
+  text-align: center;
+`;
