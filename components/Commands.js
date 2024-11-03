aa1d6f02afe50406254b4fa02130b4487f2fd7ef
Cyrus Sahar <ffenjil@proton.me>
2024-11-03 17:41:13 +0530

    Create Commands.js

---
+++ components/Commands.js
@@ -0,0 +1,16 @@
+// components/Commands.js
+import styled from 'styled-components';
+
+const Commands = () => (
+  <Section>
+    <h2>Commands</h2>
+    <p>Showcase the commands users can use with RhythmoSync.</p>
+  </Section>
+);
+
+export default Commands;
+
+const Section = styled.section`
+  padding: 2rem;
+  text-align: center;
+`;
