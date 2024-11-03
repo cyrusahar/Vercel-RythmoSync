8f034aa6b374fe8748e931507f0287a6f297013b
Cyrus Sahar <ffenjil@proton.me>
2024-11-03 17:42:01 +0530

    Create Changelog.js

---
+++ components/Changelog.js
@@ -0,0 +1,16 @@
+// components/Changelog.js
+import styled from 'styled-components';
+
+const Changelog = () => (
+  <Section>
+    <h2>Changelog</h2>
+    <p>Display recent updates and changes for RhythmoSync.</p>
+  </Section>
+);
+
+export default Changelog;
+
+const Section = styled.section`
+  padding: 2rem;
+  text-align: center;
+`;
