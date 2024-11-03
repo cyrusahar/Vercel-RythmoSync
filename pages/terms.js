493838bbc9dccb8187d52622f0f30e4bd2721fa8
Cyrus Sahar <ffenjil@proton.me>
2024-11-03 17:46:56 +0530

    Create terms.js

---
+++ pages/terms.js
@@ -0,0 +1,9 @@
+// pages/terms.js
+export default function Terms() {
+  return <div>Terms of Service content goes here.</div>;
+}
+
+// pages/privacy.js
+export default function Privacy() {
+  return <div>Privacy Policy content goes here.</div>;
+}
