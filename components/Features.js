9feacf9b17cf5c3058df07faa513e5333669171b
Cyrus Sahar <ffenjil@proton.me>
2024-11-03 17:42:33 +0530

    Create Features.js

---
+++ components/Features.js
@@ -0,0 +1,16 @@
+// components/Features.js
+import styled from 'styled-components';
+
+const Features = () => (
+  <Section>
+    <h2>Features</h2>
+    <p>Highlight the key features of RhythmoSync here.</p>
+  </Section>
+);
+
+export default Features;
+
+const Section = styled.section`
+  padding: 2rem;
+  text-align: center;
+`;
