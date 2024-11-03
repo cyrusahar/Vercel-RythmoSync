17b24403447b64d68a3d943ce6d1e0379a418d53
Cyrus Sahar <ffenjil@proton.me>
2024-11-03 17:41:39 +0530

    Create Metrics.js

---
+++ components/Metrics.js
@@ -0,0 +1,16 @@
+// components/Metrics.js
+import styled from 'styled-components';
+
+const Metrics = () => (
+  <Section>
+    <h2>Metrics</h2>
+    <p>Show server and active user count here.</p>
+  </Section>
+);
+
+export default Metrics;
+
+const Section = styled.section`
+  padding: 2rem;
+  text-align: center;
+`;
