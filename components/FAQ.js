f32a98cbe8981d031baae34f15d3fcb9d7c0e8ff
Cyrus Sahar <ffenjil@proton.me>
2024-11-03 17:44:42 +0530

    Create FAQ.js

---
+++ components/FAQ.js
@@ -0,0 +1,16 @@
+// components/FAQ.js
+import styled from 'styled-components';
+
+const FAQ = () => (
+  <Section>
+    <h2>FAQ</h2>
+    <p>Answer common questions about RhythmoSync.</p>
+  </Section>
+);
+
+export default FAQ;
+
+const Section = styled.section`
+  padding: 2rem;
+  text-align: center;
+`;
